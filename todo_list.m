f0354dfdf5a7866775c10f5dd214aff7efa0bc9a
JontAmazon <jonatan.lindholm10@gmail.com>
2019-12-10 19:14:23 +0100

    updated todo_list.m

--- todo_list.m
+++ todo_list.m
@@ -1,8 +1,18 @@
 %{
-2. Implement penalty/barrier function code (outer loop).
-    --> do task 2 and 3.
+1. Test line search with f(x) = (1 - 10^a*x)^2
 
-4. Maybe:     write some code to always check that D is P.D.
+2. Test line search with test_func.m
+
+3. Minimize rosenbrock.m
+    Try many different starting points.
+
+4. Do task 2 (main2.m) -- penalty.
+
+5. Do task 3 -- penalty.
+
+
+10. Maybe (J): (1) history
+              (2) write some code to always check that D is P.D.
               ...and a function that does D=D+epsilon*I for a series of
               incrementing epsilons until (and a bit more) D is P.D.
               ...but should we check that ALL D_j are P.D., really? [p. 82-->]
